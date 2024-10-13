c4a7f60a5d8e30fef141996fac862c95097fb57f
Martin <martin@hotmail.com.br>
2024-10-13 17:40:31 -0300

    fix: `first_seen` formatter now has two digit precision on the seconds
    Signed-off-by: Martin <martin@hotmail.com.br>

--- src/application/types.rs
+++ src/application/types.rs
@@ -1,6 +1,8 @@
 use crate::application::adapters::Satochi;
 use crate::infrastructure::api::input_dto::NodeRankingBaseDTO;
 use serde::{Deserialize, Serialize};
+use std::num::NonZeroU8;
+use time::format_description::well_known::iso8601::{Config, EncodedConfig, TimePrecision};
 use time::format_description::well_known::Iso8601;
 use time::OffsetDateTime;
 
@@ -13,11 +15,21 @@ pub struct Node {
     pub first_seen: String,
 }
 
+const ISO8601_CONFIG: EncodedConfig = {
+    let cfg_default = Config::DEFAULT;
+    let cfg = cfg_default.set_time_precision(TimePrecision::Second {
+        decimal_digits: NonZeroU8::new(2),
+    });
+    cfg.encode()
+};
+
 impl From<NodeRankingBaseDTO> for Node {
     fn from(value: NodeRankingBaseDTO) -> Self {
+        let formatter: Iso8601<ISO8601_CONFIG> = Iso8601;
+
         let first_seen = OffsetDateTime::from_unix_timestamp(value.first_seen)
             .unwrap()
-            .format(&Iso8601::DEFAULT)
+            .format(&formatter)
             .unwrap();
 
         Self {
